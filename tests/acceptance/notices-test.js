5f65a905d538df463dad50b11f82eb98aaa8734a
Michael Klein <michael@firstiwaslike.com>
2022-10-17 16:45:33 +0200

    Demonstrate how to use stub data for local-storage in testing

--- tests/acceptance/notices-test.js
+++ tests/acceptance/notices-test.js
@@ -1,12 +1,33 @@
 import { module, test } from 'qunit';
 import { visit, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'local-storage-test/tests/helpers';
+import LocalStorageService from 'local-storage-test/services/local-storage';
 
+class StorageStub {
+  constructor(data) {
+    this.data = data || new Map();
+  }
+
+  getItem(key) {
+    return this.data.get(key);
+  }
+
+  setItem(key, value) {
+    this.data.set(key, value.toString());
+  }
+}
 module('Acceptance | notices', function (hooks) {
   setupApplicationTest(hooks);
 
   hooks.beforeEach(function () {
-    localStorage.clear();
+    this.owner.register(
+      'service:local-storage',
+      class Stub extends LocalStorageService {
+        get storage() {
+          return new StorageStub();
+        }
+      }
+    );
   });
 
   test('working with notices', async function (assert) {
@@ -25,5 +46,25 @@ module('Acceptance | notices', function (hooks) {
     assert
       .dom('[data-test-notice-a]')
       .doesNotExist('notica-a is not displayed anymore');
+  });
+
+  test('when data is present in local storage', async function (assert) {
+    const data = new Map();
+    data.set('notices', 'notice-a');
+
+    this.owner.register(
+      'service:local-storage',
+      class Stub extends LocalStorageService {
+        get storage() {
+          return new StorageStub(data);
+        }
+      }
+    );
+
+    await visit('/');
+
+    assert
+      .dom('[data-test-notice-a]')
+      .doesNotExist('notica-a is not displayed');
   });
 });
